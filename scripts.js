fa6d560a2ce949dd8350230ddf6d657f3e1d9264
SmartBern <126423203+SmartBern@users.noreply.github.com>
2023-07-08 09:24:28 -0700

    visitCount not working

--- scripts.js
+++ scripts.js
@@ -1,29 +1,29 @@
 let circularProgress = document.querySelector('.circular-progress');
 let progressValue = document.querySelector('.progress-value');
 
-var counterContainer = document.querySelector('.website-counter');
-var visitCount = localStorage.getItem("page_view");
-
-//Check if page_view entry is present
-
-if (visitCount)
-{
-  visitCount = Number(visitCount) + 1;
-}
-
-else
-{
-  visitCount = 1;
-  localStorage.setItem("page_view", 1);
-}
-
-counterContainer.innerHTML = visitCount;
-
-
-function webpageViews(response) 
-{
-  document.getElementById('visits').innerText = response.value;
-}
+// var counterContainer = document.querySelector('.website-counter');
+// var visitCount = localStorage.getItem("page_view");
+// 
+Check if page_view entry is present
+// 
+// if (visitCount)
+// {
+  // visitCount = Number(visitCount) + 1;
+// }
+// 
+// else
+// {
+  // visitCount = 1;
+  // localStorage.setItem("page_view", 1);
+// }
+// 
+// counterContainer.innerHTML = visitCount;
+// 
+// 
+// function webpageViews(response) 
+// {
+  // document.getElementById('visits').innerText = response.value;
+// }
 
 
 
